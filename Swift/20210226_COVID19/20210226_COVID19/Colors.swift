c386cdf6afeb334d829c679ab1b13144061bb329
佐藤 省吾 <sato_shogo@plise.co.jp>
2021-02-28 17:00:07 +0900

    stash apply

---
+++ Swift/20210226_COVID19/20210226_COVID19/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  20210226_COVID19
+//
+//  Created by 佐藤省吾 on 2021/02/26.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    let bluePurple = UIColor(red: 87/255, green: 99/255, blue: 175/255, alpha: 1)
+}
+
+
---
+++ Swift/20210226_COVID19/20210226_COVID19/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  20210226_COVID19
+//
+//  Created by 佐藤省吾 on 2021/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let colors = Colors()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let gradiertLayer = CAGradientLayer()
+        gradiertLayer.colors = colors.
+    }
+
+
+}
+
