5d9ba75b5b113dfcf4555a83a04b185ec93f3333
佐藤 省吾 <sato_shogo@plise.co.jp>
2021-04-11 08:58:20 +0900

    通話アプリ制作開始

---
+++ 20210411_tellApp/20210411_tellApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20210411_tellApp
+//
+//  Created by 佐藤省吾 on 2021/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
