cd61e673c8e3bcf66094aedd5fb415bcb4fa17f8
佐藤 省吾 <sato_shogo@plise.co.jp>
2021-04-11 11:31:11 +0900

    Colorsファイル作成

---
+++ 20210411_tellApp/20210411_tellApp/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  20210411_tellApp
+//
+//  Created by 佐藤省吾 on 2021/04/11.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    let gray = UIColor(red: 221/255, green: 221/255, blue: 221/255, alpha: 1)
+    let skin = UIColor(red: 249/255, green: 243/255, blue: 243/255, alpha: 1)
+    let pink = UIColor(red: 247/255, green: 217/255, blue: 217/255, alpha: 1)
+    let bright = UIColor(red: 242/255, green: 82/255, blue: 135/255, alpha: 1)
+    let brack = UIColor(red: 0, green: 0, blue: 0, alpha: 0.9)
+}
